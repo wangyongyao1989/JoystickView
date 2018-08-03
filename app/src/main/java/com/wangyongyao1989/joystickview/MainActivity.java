7f9c1a57ed713b7c5f55354a6b6ddd5e432f92d0
wangyao <wangyongyao1989@163.com>
2018-08-03 10:01:49 +0800

    Initial commit

---
+++ app/src/main/java/com/wangyongyao1989/joystickview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wangyongyao1989.joystickview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
