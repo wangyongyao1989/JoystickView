6ceed2149bc79db43b2862fd9c3e265bd7d6cbe5
wangyao <wangyongyao1989@163.com>
2018-08-03 10:09:05 +0800

    添加素材及自定义View初步

---
+++ app/src/main/java/com/wangyongyao1989/joystickview/views/GameJoystickView.java
@@ -0,0 +1,33 @@
+package com.wangyongyao1989.joystickview.views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author wangyao
+ * @package com.wangyongyao1989.joystickview.views
+ * @describe TODO
+ * @date 2018/8/3
+ */
+
+public class GameJoystickView extends View {
+
+    private final String TAG = GameJoystickView.class.getName();
+
+
+    public GameJoystickView(Context context) {
+        super(context);
+    }
+
+    public GameJoystickView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GameJoystickView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
